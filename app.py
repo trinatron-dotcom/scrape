7a45378eda5b5dd85d38704401eb6c5a06017392
Solit0123 <61633743+Solit0123@users.noreply.github.com>
2025-07-31 14:44:21 -0700

    mod to include more complete header set

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ CORS(app)
 
 def scrape_text(url):
     headers = {
-        'User-Agent': 'Mozilla/5.0'
-    }
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5'
+}
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
         return f"Failed to retrieve {url} (status code: {response.status_code})"
